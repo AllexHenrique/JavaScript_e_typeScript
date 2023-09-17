128722e9b7c3a6a886af8a79c89e64f48d9fdd1d
Alex Henrique <alex00henrique@gmail.com>
2023-09-17 13:08:58 -0300

    a

---
+++ 3. JavaScript - Lógica de programação/aula_15/index.js
@@ -0,0 +1,7 @@
+let a = 'A'; // B
+let b = 'B'; // C
+let c = 'C'; // A
+const letras = [b, c, a];
+[a, b, c] = letras;
+
+console.log(a, b, c);
---
+++ 3. JavaScript - Lógica de programação/aula_17/index.js
@@ -0,0 +1,33 @@
+// console.log('Linha 0');
+// console.log('Linha 1');
+// console.log('Linha 2');
+// console.log('Linha 3');
+// console.log('Linha 4');
+// console.log('Linha 5');
+
+// i sigbifica INDEX
+// for (let i = 0; i <= 5; i++){
+//     console.log(`Linha ${i}`);
+// }
+
+// for (let i = 0; i <= 100; i+= 10){
+//     console.log(`Linha ${i}`);
+// }
+
+// for (let i = -100; i <= 500; i+= 10){
+//     console.log(`Linha ${i}`);
+// }
+
+// for (let i = 500; i >= 200; i-= 10){
+//     console.log(`Linha ${i}`);
+// }
+
+// for (let i = 0; i <= 10; i++){
+//     const par = i % 2 === 0 ? 'par' : 'ímpar';
+//     console.log(i, par);
+// }
+//                0      1        2
+const frutas =['maça', 'pêra', 'uva,'];
+for (let i = 0; i < frutas.length; i++){
+    console.log(`Índice ${i}`,frutas[i]);
+}
