685c6c9974e0f9b407f048061d46888d4d357459
Alex Henrique <alex00henrique@gmail.com>
2024-11-08 15:02:45 -0300

    .

--- Sessao_02/14_constantes/index.js
+++ Sessao_02/14_constantes/index.js
@@ -7,5 +7,14 @@
 // Não podemos modificar o valor de uma constante
 // Não UTILIZAMOS VAR, UTILIZAMOS CONST
 
-const nome = 'Alex';
-console.log(nome);
+// const nome = 'Alex';
+// console.log(nome);
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2;
+const resultadoTriplicado = resultado * 3;
+// resultadoTriplicado = resultadoTriplicado + 5;
+
+console.log( typeof primeiroNumero );
