2424b2ef10a77b2bd8a822c03646695e4b9141df
Alex Henrique <alex00henrique@gmail.com>
2024-11-08 14:51:19 -0300

    .

---
+++ Sessao_02/13_variaveis_com_let/index.js
@@ -0,0 +1,12 @@
+let nome;  // Declarou uma variavel
+nome = 'Alex';  // inicializando a variavel
+console.log(nome);
+
+
+
+// console.log(nome, 'nasceu em 1993.');
+// console.log('em 2019', nome, 'conheceu Mirelle.');
+// console.log(nome, 'casou com Mirelle em 2024.');
+// console.log('Mirelle teve 1 filho com', nome, 'em 2025');
+// console.log('O filho de', nome, 'chama Pedro.');
+
---
+++ Sessao_02/14_constantes/index.js
@@ -0,0 +1,11 @@
+// nao podemos criar constantes com palavras reservadas
+// constantes precisam ter nomes significativos
+// nao podemos comecar o nome de uma constante com um numero
+// nao podem conter espacos ou tracos
+// Utilizamos camelCase
+// Case-sensitive
+// Não podemos modificar o valor de uma constante
+// Não UTILIZAMOS VAR, UTILIZAMOS CONST
+
+const nome = 'Alex';
+console.log(nome);
