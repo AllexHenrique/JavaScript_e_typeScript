558c85dbc3979475a2b6290b01eb4594fe78027e
Alex Henrique <alex00henrique@gmail.com>
2024-11-10 12:23:24 -0300

    atual

---
+++ Sessao_02/18_letVSvar/index.js
@@ -0,0 +1,5 @@
+// ECMAScript 2015 (ES6) JavaScript
+let nome1 = 'Alex';  // NÃO FAÇA ISSO
+let nome2 = 'Henrique';
+
+console.log(nome1, nome2);
---
+++ Sessao_02/19_tipos_de_dados_promitivos/index.js
@@ -0,0 +1,7 @@
+const nome = 'Alex'; // String
+const nome1 = "Henrique"; // String
+const nome2 = `Alex`; // String
+const num1 = 100; // Number
+const num2 = 10.52; // Number
+let nomeAluno; // Undefined - Não aponta para lugar nenhum na memória
+let sobrenomeAluno = null; // Nulo - Não aponta para lugar nenhum na memória
